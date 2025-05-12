1147ece5548f5d939f08c5333a92616cfdb0e32d
Deepesh Agarawal <deepesh.a@fermionic.design>
2025-05-12 16:11:33 +0530

    moved all structs and api files from c_header folder to hal folder

--- examples/hal/bringup/helloworld.c
+++ examples/hal/bringup/helloworld.c
@@ -14,7 +14,7 @@ int main(void) {
     int a = 19;
     UartStdOutInit();
     UartPuts("Hello World. We are FermionIC Design Pvt Ltd. using SDK\n");
-    print_int_var("GPIO_ENUM: ", GPIO_FILT_EN_FILT_WIDTH_9,1);
+    print_int_var("GPIO_ENUM: ", GPIO_FILT_EN_WIDTH_9,1);
     ADC_CHNL_CFG_N_WRITE(ADC_REGS, 7, 1, 1, 1, 1, 1);
     //ADC_REGS->CHNL_CFG[7].channel_sel = 7;
     print_int_var(" a =",a,0);
