cef3a1885c28ed3a9799cd8fd57b6658bbd97b18
bilal-ali-fermionic <bilal.a@fermionic.design>
2025-05-14 11:59:42 +0530

    initial version for dma hal files, added state enums in pl230

---
+++ hal/dma/dma.c
@@ -0,0 +1,120 @@
+#include "dma.h"
+
+/*
+ *  ======== dma_init ========
+ */
+void dma_init(PL230_REGS_s *PL230_REGS, uint32_t base_ptr_address) {
+    uint32_t current_state;
+    // Wait untill current DMA completes
+    current_state = PL230_REGS->DMA_STATUS.state;
+    while(!((current_state==PL230_DMA_STATUS_STATE_IDLE) || (current_state==PL230_DMA_STATUS_STATE_STALLED) || (current_state== PL230_DMA_STATUS_STATE_DONE ))){
+        current_state = PL230_REGS->DMA_STATUS.state;
+    }
+    // Disable DMA controller for initialization
+    PL230_REGS->DMA_CFG.master_enable = 0;
+    // Set DMA data structure address
+    PL230_REGS->CTRL_BASE_PTR.packed_w = base_ptr_address;
+    // Disable all channels
+    PL230_REGS->CHNL_ENABLE_CLR.packed_w = PL230_CHNL_ENABLE_CLR_CHNL_ENABLE_CLR_MASK;
+    // Enable DMA controller
+    PL230_REGS->DMA_CFG.master_enable = 1;
+
+}
+/*
+ *  ========  dma_channel_cfg ========
+ */
+void dma_channel_cfg(DMA_REGS_s *DMA_REGS, PL230_REGS_s *PL230_REGS, dma_channel_cfg_t *dma_channel_cfg,uint8_t channel) {
+    uint32_t src_end_pointer;
+    uint32_t dst_end_pointer;
+
+    if(dma_channel_cfg->src_incr==3) {
+        src_end_pointer = dma_channel_cfg->src_addr;
+    }
+    else {
+        src_end_pointer = dma_channel_cfg->src_addr + ((1<<dma_channel_cfg->src_size)*dma_channel_cfg->total_transaction);
+    }
+    if(dma_channel_cfg->dst_incr==3) {
+        dst_end_pointer = dma_channel_cfg->dst_addr;
+    }
+    else {
+        dst_end_pointer = dma_channel_cfg->dst_addr + ((1<<dma_channel_cfg->dst_size)*dma_channel_cfg->total_transaction);
+    }
+
+    // Reading the channel configuration based pointer
+    uint32_t base_addr = PL230_REGS->CTRL_BASE_PTR.packed_w;  
+    // Calculating the pointer to the required channel configuration 
+    volatile channel_cfg_t *ch = ((channel_cfg_t *)base_addr) + channel + (dma_channel_cfg->alternate_cfg_sel ? 16 : 0);
+    volatile DMA_REPEATED_TRANSFER_CHNL_0_u *repeated_transfer_channels = (volatile DMA_REPEATED_TRANSFER_CHNL_0_u *) &DMA_REGS->REPEATED_TRANSFER_CHNL_0;
+    // Updating channel configuration 
+    ch->rsp = src_end_pointer;
+    ch->rdp = dst_end_pointer;
+    ch->ctrl.cycle_ctrl    = dma_channel_cfg->transfer_type;
+    ch->ctrl.next_useburst = dma_channel_cfg->next_useburst;
+    ch->ctrl.n_minus_1     = dma_channel_cfg->total_transaction;
+    ch->ctrl.r_power       = dma_channel_cfg->r_power;
+    ch->ctrl.src_prot_ctrl = dma_channel_cfg->src_prot_ctrl;
+    ch->ctrl.dst_prot_ctrl = dma_channel_cfg->dst_prot_ctrl;
+    ch->ctrl.src_size      = dma_channel_cfg->src_size;
+    ch->ctrl.src_inc       = dma_channel_cfg->src_incr;  
+    ch->ctrl.dst_size      = dma_channel_cfg->dst_size;
+    ch->ctrl.dst_inc       = dma_channel_cfg->dst_incr;
+    // Fill configuration
+    if(dma_channel_cfg->fill_en) {
+        DMA_REGS->FILL_MODE_CFG.fill_mode_chnl_no     = channel; 
+        DMA_REGS->FILL_MODE_CFG.fill_mode_init_val    = dma_channel_cfg->fill_init_value; 
+        DMA_REGS->FILL_MODE_CFG.fill_mode_incr_val    = dma_channel_cfg->fill_incr_value;
+        DMA_REGS->FILL_MODE.fill_mode_en = dma_channel_cfg->fill_en;
+    }
+    // Stride configuration
+    if(dma_channel_cfg->stride_en) {
+        DMA_REGS->STRIDE_MODE_CFG_0.stride_mode_chnl_no = channel;
+        DMA_REGS->STRIDE_MODE_CFG_0.src_stride_val = dma_channel_cfg->stride_src_incr;
+        DMA_REGS->STRIDE_MODE_CFG_0.dst_stride_val = dma_channel_cfg->stride_dst_incr;
+        DMA_REGS->STRIDE_MODE_CFG_0.src_inc = dma_channel_cfg->src_incr;
+        DMA_REGS->STRIDE_MODE_CFG_0.dst_inc = dma_channel_cfg->dst_incr;
+        DMA_REGS->STRIDE_MODE_CFG_1.stride_mode_src_base_addr = dma_channel_cfg->src_addr;
+        DMA_REGS->STRIDE_MODE_CFG_2.stride_mode_dst_base_addr = dma_channel_cfg->dst_addr;
+        DMA_REGS->STRIDE_MODE.stride_mode_en = dma_channel_cfg->stride_en;
+    }
+    // Reapeated Transfer
+    if(dma_channel_cfg->repeated_transfer_en) {
+        repeated_transfer_channels[channel].packed_w = dma_channel_cfg->total_transaction;
+        DMA_REGS->REPEATED_TRANSFER_ALTERNATE_SEL.packed_w = dma_channel_cfg->alternate_cfg_sel;
+        DMA_REGS->REPEATED_TRANSFER_EN.rptd_trnsfr_en = (1 << channel);
+        DMA_REGS->CFG_0.ctrl_base_ptr = PL230_REGS->CTRL_BASE_PTR.packed_w;
+        DMA_REGS->CFG_1.alt_ctrl_base_ptr = PL230_REGS->ALT_CTRL_BASE_PTR.packed_w;
+    }
+}
+/*
+ *  ========  dma_channel_en_set ========
+ */
+void dma_channel_en_set(PL230_REGS_s *PL230_REGS, uint8_t channel) {
+    PL230_REGS->CHNL_ENABLE_SET.packed_w = (1 << channel); 
+}
+/*
+ *  ========  dma_channel_en_get ========
+ */
+uint8_t dma_channel_en_get(PL230_REGS_s *PL230_REGS, uint8_t channel) {
+    return (PL230_REGS->CHNL_ENABLE_SET.packed_w >> channel) & 0x1; 
+}
+/*
+ *  ========  dma_channel_sw_trig ========
+ */
+void dma_channel_sw_trig(PL230_REGS_s *PL230_REGS, uint8_t channel) {
+    PL230_REGS->CHNL_SW_REQUEST.packed_w = (1 << channel);
+}
+/*
+ *  ========  dma_channel_priority_cfg ========
+ */
+void dma_channel_priority_cfg(DMA_REGS_s *DMA_REGS, uint8_t mode, uint16_t round_robin_mask) {
+    // If round robin arbitration is enabled 
+    if(mode==1) {
+        DMA_REGS->ARBITRATION_MASK.dma_rr_mask = round_robin_mask;
+        DMA_REGS->ARBITRATION.dma_rr_en = 1;
+    }
+    // Update Pl230 priority register
+    else {
+        DMA_REGS->ARBITRATION_MASK.dma_rr_mask = 0;
+        DMA_REGS->ARBITRATION.dma_rr_en = 0;
+    }
+}
---
+++ hal/dma/dma.h
@@ -0,0 +1,201 @@
+#include "DMA_REGS.h"
+#include "DMA_RW_API.h"
+#include "PL230_REGS.h"
+#include "PL230_RW_API.h"
+
+/*!
+ *  @brief  Defines for Transfer Configuration 
+ *
+ *  @note   Default values for the transfer configuration of a given channel 
+ */
+#define DMA_DEFAULT_SRC_ADDR                            0                                                     
+#define DMA_DEFAULT_DST_ADDR                            0
+#define DMA_DEFAULT_SRC_SIZE                            2
+#define DMA_DEFAULT_SRC_INCR                            2
+#define DMA_DEFAULT_DST_SIZE                            2
+#define DMA_DEFAULT_DST_INCR                            2
+#define DMA_DEFAULT_TRANSFER_TYPE                       2
+#define DMA_DEFAULT_TOTAL_TRANSACTION                   1
+#define DMA_DEFAULT_R_POWER                             0
+#define DMA_DEFAULT_ALTERNATE_CFG_SEL                   0
+#define DMA_DEFAULT_FILL_EN                             0
+#define DMA_DEFAULT_FILL_INCR_VALUE                     0
+#define DMA_DEFAULT_FILL_INIT_VALUE                     0
+#define DMA_DEFAULT_STRIDE_EN                           0
+#define DMA_DEFAULT_STRIDE_SRC_INCR                     0
+#define DMA_DEFAULT_STRIDE_DST_INCR                     0
+#define DMA_DEFAULT_EARLY_IRQ_REMAINING_TRANSACTION     0
+#define DMA_DEFAULT_REPEATED_TRANSFER_EN                0
+#define DMA_DEFAULT_NEXT_USEBURST                       0
+#define DMA_DEFAULT_SRC_PROT_CTRL                       0
+#define DMA_DEFAULT_DST_PROT_CTRL                       0
+
+
+/*!
+ *  @brief  Channel Number Enum 
+ */
+typedef enum {
+    DMA_CHANNEL_0  = 0,                     /*!< DMA Channel 0 */
+    DMA_CHANNEL_1  = 1,                     /*!< DMA Channel 1 */
+    DMA_CHANNEL_2  = 2,                     /*!< DMA Channel 2 */
+    DMA_CHANNEL_3  = 3,                     /*!< DMA Channel 3 */
+    DMA_CHANNEL_4  = 4,                     /*!< DMA Channel 4 */
+    DMA_CHANNEL_5  = 5,                     /*!< DMA Channel 5 */
+    DMA_CHANNEL_6  = 6,                     /*!< DMA Channel 6 */
+    DMA_CHANNEL_7  = 7,                     /*!< DMA Channel 7 */
+    DMA_CHANNEL_8  = 8,                     /*!< DMA Channel 8 */
+    DMA_CHANNEL_9  = 9,                     /*!< DMA Channel 9 */
+    DMA_CHANNEL_10 = 10,                    /*!< DMA Channel 10*/
+    DMA_CHANNEL_11 = 11,                    /*!< DMA Channel 11*/
+    DMA_CHANNEL_12 = 12,                    /*!< DMA Channel 12*/
+    DMA_CHANNEL_13 = 13,                    /*!< DMA Channel 13*/
+    DMA_CHANNEL_14 = 14,                    /*!< DMA Channel 14*/
+    DMA_CHANNEL_15 = 15                     /*!< DMA Channel 15*/
+
+} dma_channel;
+
+/*!
+ *  @brief  Channel based transfer configuration 
+ *
+ *  @note   This struct contain parameters for initializing a DMA transfer
+ *          using a given DMA channel number
+ */
+typedef struct dma_channel_cfg_t{
+    uint32_t            src_addr;                           /*!< DMA source address */
+    uint32_t            dst_addr;                           /*!< DMA destination address */
+    
+    uint16_t            total_transaction;                  /*!< DMA total number of transaction */
+    uint16_t            early_irq_remaining_transaction;    /*!< DMA early interrupt remaining transaction */
+    uint16_t            repeated_transfer_en;               /*!< DMA Repeated Transfer Enable */
+
+    uint8_t             src_size;                           /*!< DMA source size 0:byte 1:halfword 2:word */
+    uint8_t             src_incr;                           /*!< DMA source address increement */
+    uint8_t             dst_size;                           /*!< DMA destination size 0:byte 1:halfword 2:word */
+    uint8_t             dst_incr;                           /*!< DMA destination address increement */
+    uint8_t             r_power;                            /*!< DMA Arbitration rate */
+    uint8_t             next_useburst;
+    uint8_t             src_prot_ctrl;
+    uint8_t             dst_prot_ctrl;
+
+    uint8_t             alternate_cfg_sel;                  /*!< DMA alternate configuration select */
+    uint8_t             fill_en;                            /*!< DMA fill mode enable */
+    uint8_t             fill_incr_value;                    /*!< DMA fill increement value */
+    uint8_t             fill_init_value;                    /*!< DMA fill initial value */
+    uint8_t             stride_en;                          /*!< DMA stride mode enable */
+    uint8_t             stride_src_incr;                    /*!< DMA stride source address increement value */
+    uint8_t             stride_dst_incr;                    /*!< DMA stride destination address increement value */
+    uint8_t             transfer_type;                      /*!< DMA transaction type*/
+} dma_channel_cfg_t;
+
+
+/*!
+ *  @brief  Channel Control Configuration Struct 
+ */
+typedef struct ctrl_cfg_t {
+    uint32_t            cycle_ctrl     :3;
+    uint32_t            next_useburst  :1;
+    uint32_t            n_minus_1      :10;
+    uint32_t            r_power        :4;
+    uint32_t            src_prot_ctrl  :3;
+    uint32_t            dst_prot_ctrl  :3;
+    uint32_t            src_size       :2;
+    uint32_t            src_inc        :2;  
+    uint32_t            dst_size       :2;
+    uint32_t            dst_inc        :2;
+} ctrl_cfg_t;   
+
+/*!
+ *  @brief  Channel Configuration struct
+ */
+typedef struct channel_cfg {
+    uint32_t            rsp;
+    uint32_t            rdp;
+    ctrl_cfg_t          ctrl;
+    uint32_t            rsvd;
+} channel_cfg_t;
+
+/*!
+ *  @brief  Default initializer for channel_transfer_cfg struct  
+ *
+ *  @note   Make sure to updgrade this macro if 'channel_transfer_cfg_struct' is modified 
+ */
+#define CHANNEL_TRANSFER_CFG_DEFAULT {                                                                              \
+    .src_addr                           = DMA_DEFAULT_SRC_ADDR                                                     ,\
+    .dst_addr                           = DMA_DEFAULT_DST_ADDR                                                     ,\
+    .src_size                           = DMA_DEFAULT_SRC_SIZE                                                     ,\
+    .src_incr                           = DMA_DEFAULT_SRC_INCR                                                     ,\
+    .dst_size                           = DMA_DEFAULT_DST_SIZE                                                     ,\
+    .dst_incr                           = DMA_DEFAULT_DST_INCR                                                     ,\
+    .transfer_type                      = DMA_DEFAULT_TRANSFER_TYPE                                                ,\
+    .total_transaction                  = DMA_DEFAULT_TOTAL_TRANSACTION                                            ,\
+    .r_power                            = DMA_DEFAULT_R_POWER                                                      ,\
+    .next_useburst                      = DMA_DEFAULT_NEXT_USEBURST                                                ,\
+    .src_prot_ctrl                      = DMA_DEFAULT_SRC_PROT_CTRL                                                ,\
+    .dst_prot_ctrl                      = DMA_DEFAULT_DST_PROT_CTRL                                                ,\
+    .alternate_cfg_sel                  = DMA_DEFAULT_ALTERNATE_CFG_SEL                                            ,\
+    .fill_en                            = DMA_DEFAULT_FILL_EN                                                      ,\
+    .fill_incr_value                    = DMA_DEFAULT_FILL_INCR_VALUE                                              ,\
+    .fill_init_value                    = DMA_DEFAULT_FILL_INIT_VALUE                                              ,\
+    .stride_en                          = DMA_DEFAULT_STRIDE_EN                                                    ,\
+    .stride_src_incr                    = DMA_DEFAULT_STRIDE_SRC_INCR                                              ,\
+    .stride_dst_incr                    = DMA_DEFAULT_STRIDE_DST_INCR                                              ,\
+    .early_irq_remaining_transaction    = DMA_DEFAULT_EARLY_IRQ_REMAINING_TRANSACTION                              ,\
+    .repeated_transfer_en               = DMA_DEFAULT_REPEATED_TRANSFER_EN                                          \
+}
+
+/**
+ * @brief   This function initalizes the dma, waits if there is any current transaction,
+ *          set dma structure addressm disable all the channels and enables the controller
+ *
+ * @param   base_ptr : pointer to base address of the dma channel configration memory
+ *
+ * @retval  void
+ */
+void dma_init(PL230_REGS_s *PL230_REGS, uint32_t base_ptr_address);
+
+/**
+ * @brief   This function configures the DMA channel with required configuration
+ *
+ * @param   channel_transfer_cfg: struct containing all the required configuration
+ * @param   channel : the channel id for which the configuration is being done
+ *
+ * @retval  void
+ */
+void dma_channel_cfg(DMA_REGS_s *DMA_REGS, PL230_REGS_s *PL230_REGS, dma_channel_cfg_t *dma_channel_cfg,uint8_t channel);
+
+/**
+ * @brief   This function enables a particular dma channel 
+ *
+ * @param   enable : set this value to enable the channel
+ * @param   channel : channel id
+ *
+ * @retval  void
+ */
+void dma_channel_en_set(PL230_REGS_s *PL230_REGS, uint8_t channel);
+
+/**
+ * @brief   This function returns status of channel enable of a given channel 
+ *
+ * @param   channel : channel id 
+ *
+ * @retval  void
+ */
+uint8_t dma_channel_en_get(PL230_REGS_s *PL230_REGS, uint8_t channel);
+
+/**
+ * @brief    
+ *
+ * @param   
+ *
+ * @retval   
+ */
+void dma_channel_sw_trig(PL230_REGS_s *PL230_REGS, uint8_t channel);
+
+/**
+ * @brief    
+ *
+ * @param   
+ *
+ * @retval   
+ */
+void dma_channel_priority_cfg(DMA_REGS_s *DMA_REGS, uint8_t mode, uint16_t round_robin_mask);
