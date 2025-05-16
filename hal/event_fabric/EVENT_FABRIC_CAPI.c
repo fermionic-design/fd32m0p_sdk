b8f300266acd98bcd576b58189f35b7f996f768d
Sathwik Pothana <sathwik.pothana@fermionic.design>
2025-05-16 17:34:34 +0530

    event fabric CAPI checkin

--- hal/event_fabric/EVENT_FABRIC_CAPI.c
+++ hal/event_fabric/EVENT_FABRIC_CAPI.c
@@ -1,16 +1,58 @@
-#include "EVENT_FABRIC_RW_API.h"
-#include "EVENT_FABRIC_REGS.h"
-#include "EVENT_FABRIC_CAPI.h"
+#include  "EVENT_FABRIC_CAPI.h"
 
-#define EVENT_FABRIC_REGS   ((EVENT_FABRIC_REGS_s *)      0x3FFC3000)     // EVENT_FABRIC_REGS Common APB Address Space
-void EventFabricInit(){
-    EVENT_FABRIC_REGS->PWR_EN.packed_w = 0x69000001;
-    EVENT_FABRIC_REGS->RST_CTRL.packed_w = 0x69000001;
-    EVENT_FABRIC_REGS->RST_CTRL.packed_w = 0x69000000;
-    EVENT_FABRIC_REGS->CLK_CTRL.packed_w = 0x00000001;
+
+void event_fabric_pub_to_dma_channel_cfg(EVENT_FABRIC_REGS_s * registers, uint32_t pub_id,uint32_t chan_id){
+    EVENT_FABRIC_DMA_PUB_N_WRITE(registers,chan_id,pub_id);
 }
 
-//void CFG_GenricEventFabric(int gen_pub_id, int gen_pub_chan_id, int gen_sub_id, int gen_sub_chan_id ){
-//    GEN_PUB(gen_pub_id)(EVENT_FABRIC_REGS, gen_pub_chan_id);
-//    GEN_PUB(gen_sub_id)(EVENT_FABRIC_REGS, gen_sub_chan_id);
-//}
+
+void event_fabric_pub_to_sub_channel_cfg(EVENT_FABRIC_REGS_s * registers, uint32_t pub_id, uint32_t sub_id, uint32_t chan_id){
+        EVENT_FABRIC_GEN_PUB_N_WRITE(registers, pub_id, chan_id);
+        EVENT_FABRIC_GEN_SUB_N_WRITE(registers, sub_id, chan_id);
+}
+
+
+void get_dma_full_chnl_cfg(EVENT_FABRIC_REGS_s * registers, chnl_cfg * chnl){
+        uint32_t    sub_chan_id;
+        uint32_t    pub_chan_id;
+
+        for(int i=0; i<DMA_CHANNELS;i++){
+            chnl[i].pub = registers->DMA_PUB[i].packed_w;
+            chnl[i].sub = i; 
+        }
+}
+
+void get_event_fabric_full_chnl_cfg(EVENT_FABRIC_REGS_s * registers, chnl_cfg * chnl){
+        uint8_t    sub_chan_id;
+        uint8_t    pub_chan_id;
+        uint8_t    max_of_pub_sub;
+       
+        
+        for(int i=0; i< EVENT_FABRIC_CHANNELS ; i++){
+            chnl[i].pub = 0;
+            chnl[i].sub = 0;
+        }
+
+        if(EVENT_FABRIC_NUMBER_OF_PUB > EVENT_FABRIC_NUMBER_OF_SUB){
+            max_of_pub_sub = EVENT_FABRIC_NUMBER_OF_PUB;
+        }else {
+            max_of_pub_sub = EVENT_FABRIC_NUMBER_OF_SUB;
+        }
+        
+        print_int_var("chl_addr",chnl,1);
+        for(int i=0; i< max_of_pub_sub; i++){
+            if(i <= EVENT_FABRIC_NUMBER_OF_SUB ){
+                sub_chan_id = registers->GEN_SUB[i].packed_w;
+                chnl[sub_chan_id].sub = i; 
+            }
+            //print_int_var("SUB_CHAN",sub_chan_id,0);
+            //print_int_var("PUB_CHAN",pub_chan_id,0); 
+            if(i <= EVENT_FABRIC_NUMBER_OF_PUB ){
+                pub_chan_id = registers->GEN_PUB[i].packed_w;
+                chnl[pub_chan_id].pub = i;
+            }
+            //print_int_var("channel_sub",&chnl[sub_chan_id].sub,1);
+            //print_int_var("channel_pub",&chnl[pub_chan_id].pub,1);
+        }
+}
+
