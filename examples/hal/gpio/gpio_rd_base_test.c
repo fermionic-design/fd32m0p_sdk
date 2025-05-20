312729eae41db45472f7b84edbe3b8aab45223ff
Madhur Gokhale <madhur.gokhale@fermionic.design>
2025-05-20 11:47:35 +0530

    removed testbench dependency.

--- examples/hal/gpio/gpio_rd_base_test.c
+++ examples/hal/gpio/gpio_rd_base_test.c
@@ -1,3 +1,15 @@
+//////////////////////////////////////////////////////////////////////////////
+////                        GPIO RD BASE TEST                             ////
+////    DESCRIPTION:                                                      ////      
+////        This is an example test to read the inputs at the             ////
+////        pins                                                          ////
+////                                                                      ////
+////    Board Setup:                                                      ////
+////        Pins whose value is to be read should be connected to         //// 
+////        the signals which are to be read.                             ////
+////                                                                      ////
+////                                                                      ////
+//////////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdint.h> 
 #include <sys/types.h> 
@@ -13,17 +25,12 @@
 
 #define GPIO_REGS  ((GPIO_REGS_s *) 0x40010000)
 #define IOMUX_REGS  ((IOMUX_REGS_s *) 0x3FFC4000 )
-typedef struct sram_memory {
-    uint32_t mem[1024];
-} sram_memory_t;
-
-#define sram_mem_loc  ((sram_memory_t *)   0x200000F0)
 
 int main(void) {
-    int failed = 0;
-    int din = 0;
+    uint32_t failed = 0;
+    uint32_t din = 0;
     UartStdOutInit();
-    int i;
+    uint32_t i;
 
     GPIO_REGS->PWR_EN.packed_w = 0xAB000001;
     printf("Power EN Reg Written.\n");
@@ -37,39 +44,12 @@ int main(void) {
         IOMUX_PA_N_WRITE(IOMUX_REGS, i, 0, 1, 0, 0, 0, 0, 0, 1, 0);
     } 
  
-    sram_mem_loc->mem[2] = 0xAAAAAAAA;
-    sram_mem_loc->mem[0] = 0xC001C0DE;
+    while(gpio_din(GPIO_REGS) != 0);
+    din = gpio_din(GPIO_REGS);
 
-    while (sram_mem_loc->mem[3] != 0xBBBBBBBB) 
-    {
-        printf("Waiting for 0xBBBBBBBB to be written at location 3\n");
-    }
-
-    din = sram_mem_loc->mem[1];
-
-    print_int_var("din = ", din,1);
-    if(gpio_din(GPIO_REGS) == din)
-    {
-        printf("** Correct Value is set. **\n");
-    }
-    else 
-    {
-        failed++;
-        printf("Correct Value is not set. \n");
-    }
-
-    if(failed == 0)
-    {
-        printf("** TEST PASSED **\n");
-        UartPass();
-    }
-    else 
-    { 
-        printf("** TEST FAILED**\n");
-        UartFail();
-    }
-
-    sram_mem_loc->mem[2] = 0x0E9DC0DE;
+    print_int_var("din :", din, 0);
+    
+    printf("GPIO Read Test Ends\n");
 
     for (int i = 0; i< 10000 ; i++);
     UartEndSimulation();
