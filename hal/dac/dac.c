abeae5c4f2d1cfce6e80fd1d224f699261f90cd3
Madhur Gokhale <madhur.gokhale@fermionic.design>
2025-05-21 19:15:17 +0530

    renamed file names

---
+++ hal/dac/dac.c
@@ -0,0 +1,54 @@
+#include "dac.h"
+
+void dac_cfg(DAC_REGS_s *regs, dac_cfg_s dac_cfg_struct){
+    regs->CTRL0.data_fmt        = dac_cfg_struct.data_fmt;
+    regs->CTRL0.enable          = dac_cfg_struct.enable;
+    regs->CTRL0.res             = dac_cfg_struct.res;
+    regs->CTRL1.amp_en          = dac_cfg_struct.amp_en;
+    regs->CTRL1.amp_hiz         = dac_cfg_struct.amp_hiz;
+    regs->CTRL1.vrefp_sel       = dac_cfg_struct.vrefp_sel;
+    regs->CTRL1.out_en          = dac_cfg_struct.out_en;
+    regs->CTRL3.samp_tim_en     = dac_cfg_struct.samp_tim_en;
+    if(dac_cfg_struct.samp_tim_en == 1)
+    {
+        regs->CTRL3.samp_tim_rate   = 12;   //Only this value is supported.
+    }
+}
+
+void dac_dma_cfg(DAC_REGS_s *regs, dac_dma_cfg_s dac_dma_cfg_struct){
+    regs->CTRL2.fifo_th         = dac_dma_cfg_struct.fifo_th;
+    regs->CTRL2.fifo_en         = dac_dma_cfg_struct.fifo_en;
+    regs->CTRL2.fifo_trig_sel   = dac_dma_cfg_struct.fifo_trig_sel; 
+}
+
+void dac_wr_code(DAC_REGS_s *regs, uint32_t dac_code){
+    regs->DATA.dac_code = dac_code;
+}
+
+dac_cfg_s get_dac_cfg(DAC_REGS_s *regs){
+    dac_cfg_s dac_cfg_struct;  
+    dac_cfg_struct.data_fmt      = regs->CTRL0.data_fmt;
+    dac_cfg_struct.enable        = regs->CTRL0.enable;
+    dac_cfg_struct.res           = regs->CTRL0.res;        
+    dac_cfg_struct.amp_en        = regs->CTRL1.amp_en;     
+    dac_cfg_struct.amp_hiz       = regs->CTRL1.amp_hiz;    
+    dac_cfg_struct.vrefp_sel     = regs->CTRL1.vrefp_sel;  
+    dac_cfg_struct.out_en        = regs->CTRL1.out_en;     
+    dac_cfg_struct.samp_tim_en   = regs->CTRL3.samp_tim_en;
+    dac_cfg_struct.samp_tim_rate = regs->CTRL3.samp_tim_rate;
+    return dac_cfg_struct;
+}
+
+dac_dma_cfg_s get_dac_dma_cfg(DAC_REGS_s *regs){
+    dac_dma_cfg_s dac_dma_cfg_struct;
+    dac_dma_cfg_struct.fifo_th          = regs->CTRL2.fifo_th;      
+    dac_dma_cfg_struct.fifo_en          = regs->CTRL2.fifo_en;   
+    dac_dma_cfg_struct.fifo_trig_sel    = regs->CTRL2.fifo_trig_sel;
+    return dac_dma_cfg_struct;
+}
+
+uint32_t get_dac_wr_code(DAC_REGS_s *regs){
+    uint32_t dac_code;
+    dac_code = regs->DATA.dac_code;
+    return dac_code;
+}
---
+++ hal/gpio/gpio.h
@@ -0,0 +1,32 @@
+#ifndef gpio_h_include
+#define gpio_h_include
+
+#include "IOMUX_REGS.h"
+#include "IOMUX_RW_API.h"
+
+#include "GPIO_REGS.h"
+#include "GPIO_RW_API.h"
+
+void iomux_cfg(IOMUX_REGS_s *regs, IOMUX_PA_REG_s iomux_cfg_struct,  uint32_t pin_num);
+void gpio_dout_pinwise(GPIO_REGS_s *regs, uint32_t pin_num, uint32_t dout_pin_val);
+void gpio_glitch_filter_cfg(GPIO_REGS_s *regs, uint32_t pin_num, GPIO_FILT_EN_E filt_width_val);
+void gpio_intr_polarity_cfg(GPIO_REGS_s *regs, uint32_t pin_num, GPIO_INTR_POL_E intr_pol_val);
+void gpio_dma_cfg(GPIO_REGS_s *regs, uint32_t dma_wr_mask);
+uint32_t gpio_din(GPIO_REGS_s *regs);
+void gpio_dout(GPIO_REGS_s *regs, uint32_t dout);
+void gpio_dout_en(GPIO_REGS_s *regs, uint32_t dout_en);
+void gpio_set_en(GPIO_REGS_s *regs, uint32_t dout_en_set);
+void gpio_clr_en(GPIO_REGS_s *regs, uint32_t dout_en_clr) ;
+void gpio_sub_cfg(GPIO_REGS_s *regs, uint32_t sub_sel, uint32_t sub_en, uint32_t pin_action, uint32_t pin_num);
+uint32_t gpio_din_pinwise(GPIO_REGS_s *regs, uint32_t pin_num);
+void gpio_set(GPIO_REGS_s *regs, uint32_t dout_set);
+void gpio_clr(GPIO_REGS_s *regs, uint32_t dout_clr);
+void gpio_tgl(GPIO_REGS_s *regs, uint32_t dout_tgl);
+uint32_t get_gpio_glitch_filter_cfg(GPIO_REGS_s *regs,uint32_t pin_num);
+uint32_t get_gpio_intr_polarity_cfg(GPIO_REGS_s *regs, uint32_t pin_num);
+IOMUX_PA_REG_s get_gpio_iomux_cfg(IOMUX_REGS_s *regs, uint32_t pin_num);
+GPIO_SUB_CFG_REG_s get_gpio_sub_cfg(GPIO_REGS_s *regs, uint32_t sub_sel);
+uint32_t get_gpio_dma_cfg(GPIO_REGS_s *regs);
+uint32_t get_gpio_dout(GPIO_REGS_s *regs);
+uint32_t get_gpio_dout_pinwise(GPIO_REGS_s *regs, uint32_t pin_num); 
+#endif
