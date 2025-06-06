e145f4d850560912af955e33f15fe94fd4faffd3
deepesh-agarawal-fermionic <148379628+deepesh-agarawal-fermionic@users.noreply.github.com>
2025-06-06 11:59:35 +0530

    updated doxygen documentation

--- docs/html/group___a_d_c___r_e_s_u_l_t___r_e_g.js
+++ docs/html/group___a_d_c___r_e_s_u_l_t___r_e_g.js
@@ -8,5 +8,23 @@ var group___a_d_c___r_e_s_u_l_t___r_e_g =
       [ "packed_byte", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#a7592cdc652b06e91f93d52020b50790f", null ],
       [ "packed_hw", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ae8cf407a08973fe972d72b13be7caec0", null ],
       [ "packed_w", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#a5bad506cf9386a3faa9a3984aae9421e", null ]
+    ] ],
+    [ "ADC_RESULT_E", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#gaf99626b120c10df53c0296ea6efcf88f", [
+      [ "ADC_RESULT_DATA_CHNL_0", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa0c11ae7a137d7060a690f8c70999fc4b", null ],
+      [ "ADC_RESULT_DATA_CHNL_1", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fad43857da2584a3218666be653c21d364", null ],
+      [ "ADC_RESULT_DATA_CHNL_2", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fae6001263403780e6b1a5429e8fb00f7c", null ],
+      [ "ADC_RESULT_DATA_CHNL_3", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa34daf0a921b58c286cae022494938bb6", null ],
+      [ "ADC_RESULT_DATA_CHNL_4", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fabb067b588a270fc54c94f5fbe94c3a05", null ],
+      [ "ADC_RESULT_DATA_CHNL_5", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa8ad318d31e7383022ae6a6579a3b5566", null ],
+      [ "ADC_RESULT_DATA_CHNL_6", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa45c257f3ba991746eb4fd114b0ab42db", null ],
+      [ "ADC_RESULT_DATA_CHNL_7", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88faf41ea89215c9f31c4c541936a79e13e4", null ],
+      [ "ADC_RESULT_DATA_CHNL_8", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa2c7b651e628a198c6862c3b76ce3b5ad", null ],
+      [ "ADC_RESULT_DATA_CHNL_9", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa112c5e04e220573210f37c34df805916", null ],
+      [ "ADC_RESULT_DATA_CHNL_10", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fad852ffbab213c264a76681a5841bef6b", null ],
+      [ "ADC_RESULT_DATA_CHNL_11", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa175eded0162d19ca22d2184cd0b14e0d", null ],
+      [ "ADC_RESULT_DATA_CHNL_12", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa6a9fb4b80e0e8a7e6561279bb93be34d", null ],
+      [ "ADC_RESULT_DATA_CHNL_13", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fac63b3ff384901a2a0abd34639c56989a", null ],
+      [ "ADC_RESULT_DATA_CHNL_14", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fa21d5e90f22b1cd61c699788ff34d3300", null ],
+      [ "ADC_RESULT_DATA_CHNL_15", "group___a_d_c___r_e_s_u_l_t___r_e_g.html#ggaf99626b120c10df53c0296ea6efcf88fad69704bef5d64f87b3e583a535c2a487", null ]
     ] ]
 ];
