4e7c15371adbdd9bb5e45081bc7fa38b23fb18de
madhur-gokhale-fermionic <madhur.gokhale@fermionic.design>
2025-06-11 16:01:19 +0530

    Updated comparator HAL

--- hal/comp/comp.c
+++ hal/comp/comp.c
@@ -2,64 +2,70 @@
 #include "uart_stdout_mcu.h"
 
 /*
- *  ======== comp_ctrl0_cfg ========
+ *  ======== comp_cfg ========
  */
-void comp_ctrl0_cfg(COMP_REGS_s *regs, comp_ctrl0_cfg_s ctrl0_cfg){
-    regs->CTRL0.enable         = ctrl0_cfg.enable;
-    regs->CTRL0.hyst           = ctrl0_cfg.hyst;
-    regs->CTRL0.speed          = ctrl0_cfg.speed;
-    regs->CTRL0.intr_edge_sel  = ctrl0_cfg.intr_edge_sel;
+void comp_cfg(COMP_REGS_s *regs, comp_cfg_s comp_cfg){
+    regs->CTRL0.enable         = comp_cfg.enable;
+    regs->CTRL0.hyst           = comp_cfg.hyst;
+    regs->CTRL0.speed          = 0;
+    regs->CTRL0.intr_edge_sel  = comp_cfg.intr_edge_sel;
+    regs->INPUT_CTRL0.in_p_en  = comp_cfg.in_p_en;
+    regs->INPUT_CTRL0.in_p_sel = comp_cfg.in_p_sel;
+    regs->INPUT_CTRL0.in_m_en  = comp_cfg.in_m_en;
+    regs->INPUT_CTRL0.in_m_sel = comp_cfg.in_m_sel;
+    regs->INPUT_CTRL1.in_short = comp_cfg.in_short;
+    regs->INPUT_CTRL1.in_swap  = comp_cfg.in_swap;
 }
 
 /*
- *  ======== comp_input_ctrl0_cfg ========
+ *  ======== comp_win_comp_cfg ========
  */
-void comp_input_ctrl0_cfg(COMP_REGS_s *regs, comp_input_ctrl0_cfg_s input_ctrl0_cfg){
-    regs->INPUT_CTRL0.in_p_en  = input_ctrl0_cfg.in_p_en;
-    regs->INPUT_CTRL0.in_p_sel = input_ctrl0_cfg.in_p_sel;
-    regs->INPUT_CTRL0.in_m_en  = input_ctrl0_cfg.in_m_en;
-    regs->INPUT_CTRL0.in_m_sel = input_ctrl0_cfg.in_m_sel;
+void comp_win_comp_cfg(COMP_REGS_s *regs,comp_win_comp_cfg_s win_comp_cfg){
+    regs->OUT_CTRL1.win_comp_en = win_comp_cfg.enable;
+    regs->OUT_CTRL1.win_comp_sel = win_comp_cfg.win_comp_sel;
 }
 
 /*
- *  ======== comp_input_ctrl1_cfg ========
+ *  ======== comp_out_cfg ========
  */
-void comp_input_ctrl1_cfg(COMP_REGS_s *regs, comp_input_ctrl1_cfg_s input_ctrl1_cfg){
-    regs->INPUT_CTRL1.in_short = input_ctrl1_cfg.in_short;
-    regs->INPUT_CTRL1.in_swap  = input_ctrl1_cfg.in_swap;
+void comp_out_cfg(COMP_REGS_s *regs, comp_out_cfg_s comp_out_cfg){
+    regs->OUT_CTRL0.out_pol = comp_out_cfg.out_pol;
+    regs->OUT_CTRL0.samp_en = comp_out_cfg.samp_en;
 }
 
 /*
- *  ======== get_comp_ctrl0_cfg ========
+ *  ======== get_comp_out_cfg ========
  */
-comp_ctrl0_cfg_s get_comp_ctrl0_cfg(COMP_REGS_s *regs){
-    comp_ctrl0_cfg_s          ctrl0_cfg;
-    ctrl0_cfg.enable        = regs->CTRL0.enable;       
-    ctrl0_cfg.hyst          = regs->CTRL0.hyst;         
-    ctrl0_cfg.speed         = regs->CTRL0.speed;        
-    ctrl0_cfg.intr_edge_sel = regs->CTRL0.intr_edge_sel;
-    return ctrl0_cfg;
+comp_cfg_s get_comp_cfg(COMP_REGS_s *regs){
+    comp_cfg_s comp_cfg;
+    comp_cfg.enable = regs->CTRL0.enable;
+    comp_cfg.hyst   = regs->CTRL0.hyst;
+    comp_cfg.intr_edge_sel = regs->CTRL0.intr_edge_sel;
+    comp_cfg.in_p_en = regs->INPUT_CTRL0.in_p_en;
+    comp_cfg.in_p_sel = regs->INPUT_CTRL0.in_p_sel;
+    comp_cfg.in_m_en = regs->INPUT_CTRL0.in_m_en;
+    comp_cfg.in_m_sel = regs->INPUT_CTRL0.in_m_sel;
+    comp_cfg.in_short = regs->INPUT_CTRL1.in_short;
+    comp_cfg.in_swap = regs->INPUT_CTRL1.in_swap;
+    return comp_cfg;
 }
 
 /*
- *  ======== get_comp_input_ctrl0_cfg ========
+ *  ======== get_comp_win_comp_cfg ========
  */
-comp_input_ctrl0_cfg_s get_comp_input_ctrl0_cfg(COMP_REGS_s *regs){
-    comp_input_ctrl0_cfg_s input_ctrl0_cfg;
-    input_ctrl0_cfg.in_p_en  = regs->INPUT_CTRL0.in_p_en;  
-    input_ctrl0_cfg.in_p_sel = regs->INPUT_CTRL0.in_p_sel; 
-    input_ctrl0_cfg.in_m_en  = regs->INPUT_CTRL0.in_m_en;  
-    input_ctrl0_cfg.in_m_sel = regs->INPUT_CTRL0.in_m_sel;
-    return input_ctrl0_cfg;
+comp_win_comp_cfg_s get_comp_win_comp_cfg(COMP_REGS_s *regs){
+    comp_win_comp_cfg_s win_comp_cfg;
+    win_comp_cfg.enable       = regs->OUT_CTRL1.win_comp_en;
+    win_comp_cfg.win_comp_sel = regs->OUT_CTRL1.win_comp_sel;
+    return win_comp_cfg;
 }
 
 /*
- *  ======== get_comp_input_ctrl1_cfg ========
+ *  ======== get_comp_out_cfg ========
  */
-comp_input_ctrl1_cfg_s get_comp_input_ctrl1_cfg(COMP_REGS_s *regs){
-    comp_input_ctrl1_cfg_s input_ctrl1_cfg;
-    input_ctrl1_cfg.in_short = regs->INPUT_CTRL1.in_short;
-    input_ctrl1_cfg.in_swap  = regs->INPUT_CTRL1.in_swap;
-    return input_ctrl1_cfg;
+comp_out_cfg_s get_comp_out_cfg(COMP_REGS_s *regs){
+    comp_out_cfg_s comp_out_cfg;
+    comp_out_cfg.out_pol = regs->OUT_CTRL0.out_pol;
+    comp_out_cfg.samp_en = regs->OUT_CTRL0.samp_en;
+    return comp_out_cfg;
 }
-
--- hal/comp/comp.h
+++ hal/comp/comp.h
@@ -12,92 +12,130 @@
 #include "COMP_REGS.h"
 #include "COMP_RW_API.h"
 
-#include "IOMUX_REGS.h"
-#include "IOMUX_RW_API.h"
+typedef enum {
+	COMP_HYST_DISABLED  = COMP_CTRL0_HYST_DISABLED, ///< 0x0
+	COMP_HYST_LOW       = COMP_CTRL0_HYST_LOW, ///< 0x1
+	COMP_HYST_MEDIUM    = COMP_CTRL0_HYST_MEDIUM, ///< 0x2
+	COMP_HYST_HIGH      = COMP_CTRL0_HYST_HIGH, ///< 0x3
+} COMP_HYST_E;
+
+typedef enum {
+	COMP_E0_POS_E1_NEG = COMP_CTRL0_INTR_EDGE_SEL_E0_POS_E1_NEG, ///< 0x0
+	COMP_E0_NEG_E1_POS = COMP_CTRL0_INTR_EDGE_SEL_E0_NEG_E1_POS, ///< 0x1
+} COMP_INTR_EDGE_SEL_E;
+
+typedef enum {
+	COMP_IN_P_SEL_NON_INV_0       = COMP_INPUT_CTRL0_IN_P_SEL_NON_INV_0, ///< 0x0
+	COMP_IN_P_SEL_NON_INV_1       = COMP_INPUT_CTRL0_IN_P_SEL_NON_INV_1, ///< 0x1
+	COMP_IN_P_SEL_COMP0_NON_INV_2 = COMP_INPUT_CTRL0_IN_P_SEL_COMP0_NON_INV_2, ///< 0x2
+	COMP_IN_P_SEL_COMP1_NON_INV_2 = COMP_INPUT_CTRL0_IN_P_SEL_COMP1_NON_INV_2, ///< 0x2
+	COMP_IN_P_SEL_COMP0_DAC_CONN  = COMP_INPUT_CTRL0_IN_P_SEL_COMP0_DAC_CONN, ///< 0x3
+	COMP_IN_P_SEL_COMP1_DAC_CONN  = COMP_INPUT_CTRL0_IN_P_SEL_COMP1_DAC_CONN, ///< 0x3
+	COMP_IN_P_SEL_COMP0_OPA1_CONN = COMP_INPUT_CTRL0_IN_P_SEL_COMP0_OPA1_CONN, ///< 0x4
+	COMP_IN_P_SEL_COMP1_GND       = COMP_INPUT_CTRL0_IN_P_SEL_COMP1_GND, ///< 0x4
+	COMP_IN_P_SEL_COMP0_PSEL_CONN = COMP_INPUT_CTRL0_IN_P_SEL_COMP0_PSEL_CONN, ///< 0x5
+	COMP_IN_P_SEL_COMP1_PSEL_CONN = COMP_INPUT_CTRL0_IN_P_SEL_COMP1_PSEL_CONN, ///< 0x5
+} COMP_IN_P_SEL_E; 
+
+typedef enum {
+	COMP_IN_M_SEL_INV_0             = COMP_INPUT_CTRL0_IN_M_SEL_INV_0, ///< 0x0
+	COMP_IN_M_SEL_INV_1             = COMP_INPUT_CTRL0_IN_M_SEL_INV_1, ///< 0x1
+	COMP_IN_M_SEL_COMP0_INV_2       = COMP_INPUT_CTRL0_IN_M_SEL_COMP0_INV_2, ///< 0x2
+	COMP_IN_M_SEL_COMP1_INV_2       = COMP_INPUT_CTRL0_IN_M_SEL_COMP1_INV_2, ///< 0x2
+	COMP_IN_M_SEL_COMP0_OPA0_CONN   = COMP_INPUT_CTRL0_IN_M_SEL_COMP0_OPA0_CONN, ///< 0x3
+} COMP_IN_M_SEL_E;
+
+typedef enum {
+	COMP_WIN_COMP_SEL_0 = COMP_OUT_CTRL1_WIN_COMP_SEL_0, ///< 0x0
+	COMP_WIN_COMP_SEL_1 = COMP_OUT_CTRL1_WIN_COMP_SEL_1, ///< 0x1
+	COMP_WIN_COMP_SEL_2 = COMP_OUT_CTRL1_WIN_COMP_SEL_2, ///< 0x2
+} COMP_WIN_COMP_SEL_E;
+
+typedef enum {
+	COMP_OUT_POL_NON_INV = COMP_OUT_CTRL0_OUT_POL_NON_INV, ///< 0x0
+	COMP_OUT_POL_INV = COMP_OUT_CTRL0_OUT_POL_INV, ///< 0x1
+} COMP_OUT_POL_E;
 
 /*!
- *  @brief  COMP CTRL0 Configuration Struct
+ *  @brief  COMP Configuration Struct
  */
 typedef struct {
     uint32_t enable;
-    uint32_t speed;
-    uint32_t hyst;
-    uint32_t intr_edge_sel;
-} comp_ctrl0_cfg_s;
-
-/*!
- *  @brief  COMP INPUT_CTRL0 Configuration Struct
- */
-typedef struct {
-    uint32_t in_p_en;
+    COMP_HYST_E hyst;
+    COMP_INTR_EDGE_SEL_E intr_edge_sel;
+    COMP_IN_P_SEL_E in_p_en;
     uint32_t in_p_sel;
     uint32_t in_m_en;
-    uint32_t in_m_sel;
-} comp_input_ctrl0_cfg_s;
-
-/*!
- *  @brief  COMP INPUT_CTRL1 Configuration Struct
- */
-typedef struct {
+    COMP_IN_M_SEL_E in_m_sel;
     uint32_t in_short;
     uint32_t in_swap;
-} comp_input_ctrl1_cfg_s;
+} comp_cfg_s;
+
+typedef struct {
+    uint32_t enable;
+    COMP_WIN_COMP_SEL_E win_comp_sel;
+} comp_win_comp_cfg_s;
+
+typedef struct {
+    uint32_t samp_en;
+    COMP_OUT_POL_E out_pol;
+} comp_out_cfg_s;
 
 /**
- * @brief   This function configures ______________.
+ * @brief   This function configures comparator.
  *
  * @param   regs : pointer to the COMP register space
- * @param   ctrl0_cfg  : ctrl0 configuration struct
+ * @param   comp_cfg  : comp configuration struct
  *
  * @retval  void
  */
-void comp_ctrl0_cfg(COMP_REGS_s *regs, comp_ctrl0_cfg_s ctrl0_cfg);
+void comp_cfg(COMP_REGS_s *regs, comp_cfg_s comp_cfg);
 
 /**
- * @brief   This function configures ______________.
+ * @brief   This function configures window comparator operation in comparator.
  *
  * @param   regs : pointer to the COMP register space
- * @param   input_ctrl0_cfg : input ctrl0 configuration struct
+ * @param   win_comp_cfg  : window comp configuration struct
  *
  * @retval  void
  */
-void comp_input_ctrl0_cfg(COMP_REGS_s *regs, comp_input_ctrl0_cfg_s input_ctrl0_cfg);
+void comp_win_comp_cfg(COMP_REGS_s *regs,comp_win_comp_cfg_s win_comp_cfg);
 
 /**
- * @brief   This function configures ______________.
+ * @brief   This function configures output of comparator.
  *
  * @param   regs : pointer to the COMP register space
- * @param   input_ctrl1_cfg : input ctrl1 configuration struct
+ * @param   comp_out_cfg  : comparator output configuration struct
  *
  * @retval  void
  */
-void comp_input_ctrl1_cfg(COMP_REGS_s *regs, comp_input_ctrl1_cfg_s input_ctrl1_cfg);
+void comp_out_cfg(COMP_REGS_s *regs, comp_out_cfg_s comp_out_cfg);
 
 /**
- * @brief   This function returns ______________.
+ * @brief   This function returns comparator configuration.
  *
  * @param   regs : pointer to the COMP register space
  *
- * @retval  ctrl0 config struct
+ * @retval  Comparator configuration
  */
-comp_ctrl0_cfg_s get_comp_ctrl0_cfg(COMP_REGS_s *regs);
+comp_cfg_s get_comp_cfg(COMP_REGS_s *regs);
 
 /**
- * @brief   This function returns ______________.
+ * @brief   This function returns window comparator configuration in comparator.
  *
  * @param   regs : pointer to the COMP register space
  *
- * @retval  input_ctrl0 config struct
+ * @retval  Window comparator configuration
  */
-comp_input_ctrl0_cfg_s get_comp_input_ctrl0_cfg(COMP_REGS_s *regs);
+comp_win_comp_cfg_s get_comp_win_comp_cfg(COMP_REGS_s *regs);
 
 /**
- * @brief   This function returns ______________.
+ * @brief   This function returns output configuration of comparator.
  *
  * @param   regs : pointer to the COMP register space
  *
- * @retval  input_ctrl1 config struct
+ * @retval  Comparator output configuration
  */
-comp_input_ctrl1_cfg_s get_comp_input_ctrl1_cfg(COMP_REGS_s *regs);
+comp_out_cfg_s get_comp_out_cfg(COMP_REGS_s *regs);
 
 #endif
